da5ad425c7bd0b284d0f2ff90571a3f0f53b3812
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-05-11 19:06:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app = FastAPI(title="Financial Health Analyzer")
 # Static files & templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+templates.env.cache = None
 
 # Analysis Engine instance
 engine = AnalysisEngine()
